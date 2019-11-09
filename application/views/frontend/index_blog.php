337cc2149ddc2e050022a0ed7bedf7981e2627d5
Purwa Sabrang Ramadhan <26319163+TheSkinnyRat@users.noreply.github.com>
2019-11-09 22:26:32 +0700

    delete gtm

--- application/views/frontend/index_blog.php
+++ application/views/frontend/index_blog.php
@@ -19,13 +19,6 @@
 
     gtag('config', 'UA-146827817-1');
   </script>
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TMCBLP8');</script>
-  <!-- End Google Tag Manager -->
 
   <!-- Progress bar loading -->
   <script src="<?php echo base_url('assets/frontend/vendor/pace/pace.min.js') ?>"></script>
