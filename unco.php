050d376aee36538bf6a35f4850f482b550217183
Purwa Sabrang Ramadhan <26319163+TheSkinnyRat@users.noreply.github.com>
2020-01-30 23:10:51 +0700

    change unco.php page

--- unco.php
+++ unco.php
@@ -4,23 +4,55 @@
         <title>Site is down for maintenance</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/frontend/vendor/animate/animate.min.css">
         <style type="text/css">
-            body { text-align: center; padding: 10%; font: 20px Helvetica, sans-serif; color: #333; }
-            h1 { font-size: 50px; margin: 0; }
-            article { display: block; text-align: left; max-width: 650px; margin: 0 auto; }
+            body { text-align: center; padding: 10%; font: 20px Helvetica, sans-serif; color: #333; background-color: #e0e0e0; }
+            h1 { font-size: 30px; margin: 0; }
+            article { display: block; text-align: center; max-width: 650px; margin: 0 auto; }
             a { color: #dc8100; text-decoration: none; }
             a:hover { color: #333; text-decoration: none; }
             @media only screen and (max-width : 480px) {
-                h1 { font-size: 40px; }
+                h1 { font-size: 20px; }
             }
         </style>
     </head>
     <body>
         <article>
-            <h1>UNDER MAINTENANCE!</h1>
-            <p>Sorry! This website is not available! </p>
-            <p>Scheduled maintenance is currently in progress. Please check back soon..</p>
-            <p id="signature">&mdash; &copy; BY <a href="https://www.instagram.com/the.skinny.rat" target="_blank">スキニーラット。</a> 2019</p>
+            <div class="animated infinite heartBeat" style="font-size: 75pt;">❤️</div>
+			<br/>
+			<br/>
+			<h1 style="">UNDER MAINTENANCE</h1>
+            <p><a href="https://nyrat.id/admin">Run Debug Mode</a></p>
+			<p><a href="#" onclick="location.reload(true);">Refresh Page</a></p>
+			<hr/>
+            <!-- start blog_1 Ads-->
+        <ins class="adsbygoogle"
+          style="display:inline-block; min-width:300px; max-width:1000px; width:100%; height:90px"
+          data-ad-client="ca-pub-1587350440311353"
+          data-ad-slot="1263661778"
+          data-full-width-responsive="false"></ins>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+        <!-- end blog_1 Ads-->
+		<!-- start blog_1 Ads-->
+            <ins class="adsbygoogle"
+            style="display:block;"
+            data-ad-client="ca-pub-1587350440311353"
+            data-ad-slot="6914962176"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+            <!-- end blog_1 Ads-->
+			<!-- start blog_1 Ads-->
+        <ins class="adsbygoogle"
+          style="display:inline-block; min-width:300px; max-width:1000px; width:100%; height:90px"
+          data-ad-client="ca-pub-1587350440311353"
+          data-ad-slot="7952977603"
+          data-full-width-responsive="false"></ins>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+        <!-- end blog_1 Ads-->
         </article>
     </body>
 </html>
