268b7a4eb29270cfa2628b445e780254ed6b1b30
Purwa Sabrang Ramadhan <26319163+TheSkinnyRat@users.noreply.github.com>
2020-01-31 06:46:56 +0700

    change unco.php page r3

--- unco.php
+++ unco.php
@@ -34,16 +34,7 @@
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
         <!-- end blog_1 Ads-->
-		<!-- start blog_1 Ads-->
-            <ins class="adsbygoogle"
-            style="display:block;"
-            data-ad-client="ca-pub-1587350440311353"
-            data-ad-slot="6914962176"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
-            <!-- end blog_1 Ads-->
+		
 			<!-- start blog_1 Ads-->
         <ins class="adsbygoogle"
           style="display:inline-block; min-width:300px; max-width:1000px; width:100%; height:90px"
